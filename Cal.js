cd73ae21292d506d5ba0519d01e20bde0bf6e02d
hemanthmul007 <152464504+hemanthmul007@users.noreply.github.com>
2024-10-18 12:54:03 +0530

    2300033113_MulagadaVenkataHemanth

---
+++ Cal.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function div(a,b){
+    return a/b;
+}
+let a=30;
+let b=10;
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(div(a,b));
---
+++ info.js
@@ -0,0 +1,4 @@
+var a=2300033113;
+let b='Hemanth';
+let c='CSE';
+console.log(a,b,c);
